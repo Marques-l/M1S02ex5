faee87c6a8c6a22aa4466a9339827397be277ce9
larissa <larissamarques.ufam@gmail.com>
2023-02-20 15:25:59 -0300

    Ajuste

--- M1S02ex5/Cobrar.cs
+++ M1S02ex5/Cobrar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,23 @@ namespace M1S02ex5
 {
     internal class Cobrar
     {
+        private decimal valor;
+        private decimal multa;
+        public decimal soma; 
+       public void Calcular(decimal Valor, decimal Multa)
+        {
+            Valor = valor;
+            Multa = multa; 
+        }
+        private void CalcularMulta()
+        {
+            soma = valor + multa;
+            return; 
+        }
+        public void Calcular()
+        {
+            CalcularMulta(); 
+        }
     }
+    
 }
--- M1S02ex5/Program.cs
+++ M1S02ex5/Program.cs
@@ -1,1 +1,3 @@
-﻿using M1S02ex5; 
+﻿using M1S02ex5;
+Cobrar novaCobranca = new Cobrar(500, 399); 
+Console.WriteLine(novaCobranca);
